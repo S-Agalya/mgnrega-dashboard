15de74be66840035411cad458c968157c668d46e
Kirubakaran S <kiruba5701@gmail.com>
2025-11-01 21:57:02 +0530

    fixed sync issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,7 +9,9 @@ const pool = new Pool({
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
   port: process.env.PG_PORT,
-  ssl: false
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect()
